22424aa54d64fe37cb2fb1dbaee46d9e3d0240c7
Am10aN16 <2020071013@mmmut.ac.in>
2021-08-30 15:32:28 +0530

    aman

---
+++ script.js
@@ -0,0 +1,54 @@
+// javascript for navigation bar effectss on scroll
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+// js of responsive navigation sidebar menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems= document.querySelectorAll(".navigation a");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener("click", ()=> {
+        menuBtn.classList.remove("active");
+        navigation.classList.remove("active");
+    });
+});
+
+// js for scroll to top btn
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+
+window.addEventListener("scroll", function(){
+    scrollBtn.classList.toggle("active", window.scrollY >500);
+});
+
+
+// js for scroll back to top on click
+scrollBtn.addEventListener("click", () =>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+// js for reveal website Element on scroll
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+
+    for(var i = 0; i<reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }
+    }
+}
